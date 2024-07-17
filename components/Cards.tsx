330f59240a28aca7f45b4315cd77774020ca3571
Priyum Agrawal <priyumagrawal@gmail.com>
2024-07-17 06:21:28 +0530

    add meeting actions

---
+++ components/Cards.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react'
+
+function CardsContainer({children, height, className, width}: {children: ReactNode, height: Number}) {
+  return (
+    <div className={["cards-container", className || null].join(" ")} style={{"minHeight": height || null, "minWidth": width || null}}>
+        {children}
+    </div>
+  )
+}
+
+export default CardsContainer
+
+export function Card ({children, backgroundImage, className, width}: {children: ReactNode, backgroundImage: string}) {
+    return <div className={["card", className || null].join(" ")} style={{"backgroundImage": backgroundImage ? `url(/${backgroundImage})` : null, minWidth: width || null}}>{children}</div>
+}
