9befedfe3d1e4d1d6759b2d4f14bc8d11aa503ea
Priyum Agrawal <priyumagrawal@gmail.com>
2024-07-19 07:24:13 +0530

    update

--- constants/index.tsx
+++ constants/index.tsx
@@ -30,5 +30,8 @@ export const sideBarItems: Item[] = [
 
 export const avatarImages = [
     '/images/avatar-1.jpeg',
-
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
   ];
