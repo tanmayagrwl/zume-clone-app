2d70e5344e04ecbbaa0fada2bc758f08279cf16e
Priyum Agrawal <priyumagrawal@gmail.com>
2024-07-19 17:59:54 +0530

    update

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image';
+import {Hourglass} from 'react-loader-spinner';
 
 const Loader = () => {
   return (
     <div className="flex-center h-screen w-full">
-      <Image
-        src="/loading-circle.svg"
-        alt="Loading..."
-        width={50}
-        height={50}
+      <Hourglass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        colors={['#306cce', '#72a1ed']}
       />
-    </div>
+  </div>
   );
 };
 
